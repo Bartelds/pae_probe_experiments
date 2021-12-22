2cd0bfd84ac1ff53dfd24ac52eb11278f6097e77
Martijn Bartelds <m.bartelds@rug.nl>
2021-12-22 11:36:38 +0100

    Update plot code

--- bin/plot_w2v2.py
+++ bin/plot_w2v2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import pandas as pd
+import numpy as np
 import seaborn as sns
 
 from argparse import ArgumentParser
@@ -8,53 +9,63 @@ from glob import glob
 from pathlib import Path
 
 
-def process_data(outputs, models):
+def process_data(outputs, models, eval, save):
     """Reads stdout and returns it as a DataFrame"""
 
     scores = []
     ft_method = []
     clfs = []
     columns = []
+    corpus = []
     for output in outputs:
         clf = output.split("_")[-1][:-7]
         with open(output, "r") as f:
             lines = f.readlines()
             for line in lines:
-                if line.strip()[0] in ["t", "0"] or line.strip().split()[0] in models:
-                    lsts = line.split()
+                lsts = line.strip().split()
+                if len(lsts) > 0:
                     if lsts[0] == "0":
                         scores.append(lsts[1:])
+                        if lsts[1] == lsts[2]:
+                            corpus.append(lsts[1])
                     if lsts[0] in models:
                         ft_method.append(lsts)
                     if lsts[0] =="train":
                         columns.append(lsts)
                         clfs.append(clf)
-    
+
     ft_model = [sublist[0] for sublist in ft_method]
     ft_layer = ["-".join(sublist[1:]) for sublist in ft_method]
+    scores = [score for score in scores if score[0] == lsts[1]]
     df = pd.DataFrame(scores, columns=columns[0])
     df["method"] = ft_model
     df["layer"] = ft_layer
     df["classifier"] = clfs
     df[["acc", "precision", "recall", "f1"]] = df[["acc", "precision", "recall", "f1"]].apply(pd.to_numeric)
+    
+    if save:
+        save_dir = "../experiments/wav2vec2.0/raw/" + str(list(set(corpus))[0]) + "/"
+        os.makedirs(save_dir, exist_ok=True)
+        df.to_csv(save_dir + eval + "_" + "_".join(models) + ".csv", index=False)
 
-    return df
+    return df, str(list(set(corpus))[0])
 
 
-def plot_scores(df, output_dir, eval):
+def plot_scores(df, corpus, output_dir, eval):
     """Plots phone classification performance for each model in DataFrame. Plots F1 score by default"""
 
-    output_dir = Path(output_dir, eval)
+    output_dir = Path(output_dir, corpus, eval)
     print(output_dir)
     os.makedirs(output_dir, exist_ok=True)
-
+    
     df_group = df.groupby("method")
     for index, group in df_group:
         print(f"Plotting results for {index}")
-        f = sns.lineplot(data=group, x="layer", y="f1", hue="classifier").set_title(f"Phone-classification {eval} scores for {index} on TIMIT")
+        f = sns.lineplot(data=group, x="layer", y="f1", hue="classifier").set_title(f"Phone-classification {eval} scores for {index} on {group['train'].to_list()[0]}")
         f.figure.savefig(str(output_dir) + f"/{index}_{eval}.png")
         f.figure.clf()
     print(f"Plots stored at {output_dir}")
+
 
 def main():
     parser = ArgumentParser(
@@ -63,13 +74,14 @@ def main():
     )
     parser.add_argument("-e", "--eval", default="f1", choices=["acc", "precision", "recall", "f1"])
     parser.add_argument("-o", "--output_dir", default="../experiments/wav2vec2.0/plots/")
+    parser.add_argument("-s", "--save", default=False)
     args = parser.parse_args()
 
     outputs = glob("../experiments/wav2vec2.0/logs/*.stdout")
-    models = ["wav2vec2-large-960h", "wav2vec2-large-xlsr-53"]
-
-    df = process_data(outputs, models)
-    plot_scores(df, args.output_dir, args.eval)
+    models = ["wav2vec2-large", "wav2vec2-large-960h"]
+    
+    df, corpus = process_data(outputs, models, args.eval, args.output_dir, args.save)
+    plot_scores(df, corpus, args.output_dir, args.eval)
 
 if __name__ == "__main__":
     main()
